90bc9e23215220c2d2dbfe3215de14efa3ab8189
Raafia Fatima <raafia.fatima.2004@gmail.com>
2025-10-21 09:24:47 +0500

    fix: fixed Payment Method Layout

--- src/views/CheckoutPage/ui/PaymentSection.jsx
+++ src/views/CheckoutPage/ui/PaymentSection.jsx
@@ -1,27 +1,16 @@
-import React, { useState } from 'react'
-import StripeCheckout from './StripeCheckout';
-import { useCart } from '../../../context/Cart/CartContext';
-import Button from '../../../components/ui/Buttons/Button';
-import { div } from 'framer-motion/client';
+import React, { useState } from "react";
+import StripeCheckout from "./StripeCheckout";
+import { useCart } from "../../../context/Cart/CartContext";
+import Button from "../../../components/ui/Buttons/Button";
+import { div } from "framer-motion/client";
 
 function PaymentSection() {
-    const {cartItems} = useCart()
-    const [showStripe, setShowStripe] = useState(false);
+  const { cartItems } = useCart();
   return (
     <div>
-      <div>
-      {!showStripe ? (
-        <Button 
-        onClick={() => setShowStripe(true)}
-        variant='secondary'>
-            Pay with card
-        </Button>
-      ) : (
-        <StripeCheckout cartItems={cartItems} />
-      )}
+      <StripeCheckout cartItems={cartItems} />
     </div>
-    </div>
-  )
+  );
 }
 
-export default PaymentSection
+export default PaymentSection;
